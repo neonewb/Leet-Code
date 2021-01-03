09714e11edd9d1607fd06681a72e2e455affceed
neonewb <imperror555@gmail.com>
2021-01-03 21:37:19 +0700

    solve decodeMorse

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,40 @@
-import { spinWords } from './index'
+import { countDublicates, decodeMorse, runningSum, spinWords } from './index'
+
+describe('runningSum', () => {
+  test('[1,2,3,4] => [1,3,6,10]', () => {
+    expect(runningSum([1,2,3,4])).toStrictEqual([1,3,6,10])
+  })
+})
+
+describe('DecodeMorse', () => {
+  test('HEY JUDE', () => {
+    expect(decodeMorse('.... . -.--   .--- ..- -.. .')).toBe('HEY JUDE')
+  })
+})
+
+describe('Count dublicates', () => {
+  test('abcde 0', () => {
+    expect(countDublicates('abcde')).toBe(0)
+  })
+  test('aabbcde 2', () => {
+    expect(countDublicates('aabbcde')).toBe(2)
+  })
+  test('aabBcde 2', () => {
+    expect(countDublicates('aabBcde')).toBe(2)
+  })
+  test('indivisibility 1', () => {
+    expect(countDublicates('indivisibility')).toBe(1)
+  })
+  test('Indivisibilities 2', () => {
+    expect(countDublicates('Indivisibilities')).toBe(2)
+  })
+  test('aA11 2', () => {
+    expect(countDublicates('aA11')).toBe(2)
+  })
+  test('ABBA 2', () => {
+    expect(countDublicates('ABBA')).toBe(2)
+  })
+})
 
 describe('Spin Words', () => {
   test('Spin Welcome', () => {
--- index.ts
+++ index.ts
@@ -1,3 +1,78 @@
+export const runningSum = (nums: number[]): number[] => {
+  nums.reduce((a,c,i) => nums[i] += a)
+  return nums
+}
+
+const alphabetMorse = {
+  '-----': '0',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '/': ' ',
+  '-·-·--': '!',
+  '·-·-·-': '.',
+  '--··--': ',',
+} as const
+
+export const decodeMorse = (morseCode: string) => {
+  return morseCode
+    .split(' ')
+    .reduce((prev, char): string => prev + (alphabetMorse[char] || ' '), '')
+    .toUpperCase()
+    .replace(/  /g, ' ')
+    .trim()
+}
+
+export const countDublicates = (str: string) => {
+  console.log(str.match(/([^])\1+/g))
+
+  let result: number = 0
+  let charsResolved: string[] = []
+  const chars = str.toLowerCase().split('')
+  chars.forEach((char) => {
+    if (!charsResolved.includes(char)) {
+      const matches = chars.filter((c) => c === char)
+      if (matches.length > 1) {
+        result += 1
+        charsResolved.push(char)
+      }
+    }
+  })
+  return result
+}
+
 export const spinWords = (str: string) => {
   let result: String[] = []
   const words = str.split(' ')
