07549c4f915cc270d6e43c559cfc8f9b05dff81b
neonewb <imperror555@gmail.com>
2021-04-19 23:07:43 +0700

    sum BinaryTree

---
+++ src/problemsets/binaryTree/binaryTree.ts
@@ -0,0 +1,13 @@
+type BinaryTree = {
+  value: number
+  left?: BinaryTree
+  right?: BinaryTree
+}
+
+export const sumTree = (BTree: BinaryTree) => {
+  let result = BTree.value
+  if (BTree.left) result += sumTree(BTree.left)
+  if (BTree.right) result += sumTree(BTree.right)
+  return result
+}
+
